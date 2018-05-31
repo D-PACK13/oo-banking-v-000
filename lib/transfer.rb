e6b3c391969e3ef37702648be5f25205882ecfe8
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-31 14:19:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,12 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
+  
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      
     
   
 end
